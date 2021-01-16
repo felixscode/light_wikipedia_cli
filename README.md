e627f8bbbc4004b61afa66047c22ebc27c9b96d3
Felix <felixschelling@gmx.de>
2021-01-16 21:50:37 +0100

    finshed code and readme

---
+++ README.md
@@ -0,0 +1,21 @@
+# Wikipedia Term
+
+Command Line tools for wikipedia written in python:
+Author Felix Schelling 16.01.2021
+License: MIT
+
+Options:
+  -h, --help            show this help message and exit
+  -s SEARCH, --search=SEARCH
+                        specify an search query if empty or not providen
+                        random article is choosen
+  -p, --pdf             renders wikipage to pdf and opens pdf viewer
+  -f, --full            show initre content
+  -d, --deutsch         search and show in german
+
+Dependcies:
+Python 3
+zatuha 
+pip:
+    wikipedia
+    pdfkit
--- wikipeda_term.py
+++ wikipeda_term.py
@@ -1,6 +1,7 @@
 """
 Wikipedia command line tool
 """
+from genericpath import exists
 import os
 import optparse
 import wikipedia as wp
@@ -14,14 +15,18 @@ class wikipedia_term:
         pass
 
     def search_article(self,query,num_suggestions):
-        suggestions = wp.search(query,num_suggestions)
+        suggestions = wp.search(query)
         print('Suggestions:')
         print(80*'-')
         for num,sugg in enumerate(suggestions):
             print("{}: {}".format(num+1,sugg))
         print(80*'-')
         choosen_sugg = input("Choose Aritcle (1-{}):".format(len(suggestions)))
-        return wp.page(suggestions[int(choosen_sugg)-1])
+        try:
+            return wp.page(suggestions[int(choosen_sugg)-1],auto_suggest=False)
+        except:
+            print('Sorry bad option page couldnt be found. Searching again for selected option')
+            self.search_article(suggestions[int(choosen_sugg)-1],num_suggestions)
 
     def summarize(self,page):
         return (page.title,page.summary)
@@ -30,7 +35,10 @@ class wikipedia_term:
         return page.title,page.content
 
     def get_random_article(self):
-        return wp.page(wp.random())
+        try:
+            return wp.page(wp.random())
+        except:
+            return self.get_random_article()
 
     def term_print(self, info_tupel):
         name,article = info_tupel
@@ -48,16 +56,35 @@ class wikipedia_term:
         print(str(path.absolute()))
         subprocess.call([pdf_viewer, str(path.absolute())])
 
+def add_parser_opts(): 
+    parser = optparse.OptionParser()
+    parser.add_option("--search","-s",type = 'string',help = "specify an search query if empty or not providen random article is choosen" )
+    parser.add_option("--pdf","-p",action = "store_true",default = False,help = "renders wikipage to pdf and opens pdf viewer")
+    parser.add_option("--full","-f",action = "store_true",default = False,help = "show initre content")
+    parser.add_option("--deutsch","-d",action = "store_true",default = False,help = "search and show in german")
+    return parser
 
 def main():
-    p = optparse.OptionParser()
-    p.addoption('‑‑person', '‑p', default="world")
-    options, arguments = p.parseargs()
-    print('Hello %s' % options.person)
+    wpt = wikipedia_term()
+    parser = add_parser_opts()
+    (options, args) = parser.parse_args() 
+    if options.deutsch:
+        wp.set_lang('de')
+    if options.search != None:
+        page = wpt.search_article(options.search,5)
+    else:
+        page = wpt.get_random_article()
+    
+    if options.pdf:
+        wpt.render(page)
+    elif options.full:
+        wpt.term_print(wpt.get_content(page))
+    else: 
+        wpt.term_print(wpt.summarize(page))
+
 
 if __name__ == '__main__':
-    wpt = wikipedia_term()
-    wpt.render(wpt.get_random_article())
+    main()
 
 
     
